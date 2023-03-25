0bfd2e58e654163eed719a70138091c60ab96af7
Tuna <tunakrdnz@gmail.com>
2023-03-25 20:42:18 +0300

    gave a new name to a folder

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { UtilityBar } from "./Header/UtilityBar";
 import { MainBar } from "./Header/MainBar";
 import { ImageSection } from "./Header/ImageSection";
 import { LayoutSection } from "./Header/LayoutSection";
-import { TopBionicle } from "./Body/TopBionicle";
+import { TopBionicle } from "./Body-Part2/TopBionicle";
 import { FooterIndex } from "./Footer/FooterIndex";
 import "./index.css";
-import { BionicleFinal } from "./Body/BionicleFinal";
+import { BionicleFinal } from "./Body-Part2/BionicleFinal";
 
 export class App extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import "./Body/style.css";
+import "./Body-Part2/style.css";
 
 const app = <App />;
 const root = document.getElementById("root");
